5331a720f9cd7402c7706f8604c6a8008b4eb4b2
Luca Bailey <mail@photon.garden>
2023-12-01 23:24:05 -0500

    Refactor the landing page.

---
+++ previous-version/public/src/virtual_corals/header.js
@@ -0,0 +1,137 @@
+'use strict';
+
+
+var animation = window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                window.oRequestAnimationFrame;
+
+(function() {
+    var width, height
+
+    var n_frames = [97, 103];
+    var img_aspect = [1920/1080, 1081/1358 ];
+    var images = [null, null];
+    var size = 0;
+    var others_loaded = false;
+
+    var video_scroll_length = 5000;
+    var after_padding = 1000;
+
+    var $container = null;
+    var $img = null;
+    var $footer = $('#footer');
+    var last_frame = null;
+    var last_scroll = null;
+
+    function on_scroll() {
+        var frame, y, percent, src;
+
+        var scroll = window.pageYOffset;
+        var top = $('img#haeckal').offset().top;
+
+        if (scroll == last_scroll){
+            return; //window.requestAnimationFrame(on_scroll);
+        }
+        last_scroll = scroll;
+
+        percent = (scroll / top) * 1.1;
+        frame = Math.min(Math.floor(percent * n_frames[size]), n_frames[size] - 1);
+        frame = (frame<0) ? 0 : frame; // Don't allow negative.
+
+        if (frame !== last_frame) {
+            last_frame = frame;
+            src = images[ size ][ frame ].src
+            $img[ 0 ].src = images[ size ][ frame ].src;
+        }
+        if (percent > 1.2) {
+            $footer.show()
+        } else {
+            $footer.hide()
+        }
+        // return window.requestAnimationFrame(on_scroll);
+    };
+
+    function load_images() {
+        var img;
+        if (images[ size ]) {
+            return;
+        }
+
+        var counter = [];
+        var n = n_frames[ size ];
+        console.log('load images', size, n);
+        console.time('load_header');
+
+        images[ size ] = [];
+
+        function incrementCounter() {
+            counter++;
+            // resize()
+            // scroll()
+            if ( counter == n-1 ) {
+                console.timeEnd('load_header');
+                if (!others_loaded) {
+                    window.load_all_images();
+                    others_loaded = true;
+                }
+
+            } else if (counter == 1) {
+                $img.show()
+            }
+            // last_scroll = -1;
+        }
+
+        for (var i = 1; i <= n; i++) {
+            img = new Image();
+            if (size == 0){
+                img.src = '/imgs/corals/haeckal_half/frame-' + i + '.jpeg';
+                images[size].push(img);
+            } else {
+                img.src = '/imgs/corals/haeckal_tall/frame-' + i + '.jpeg';
+                images[size].push(img);
+            }
+            img.addEventListener( 'load', incrementCounter, false );
+        }
+    }
+
+    function resize() {
+
+        var $title_container = $('#title_container');
+
+        // return
+        height = $(window).height();
+        width = $(window).width();
+        size = width>height ? 0:1
+        load_images();
+
+        var padding_top = 0;
+
+        if (width / img_aspect[size] > height) {
+            // Limiting factor is height.
+            var img_width = height * img_aspect[size];
+            $img.css('height', '100vh');
+            $img.css('width', 'auto');
+            // $img.css('padding-left', (width - img_width) / 2);
+            // $img.css('padding-top', 0);
+        } else {
+            // Limiting factor is width.
+            var img_height = width / img_aspect[size];
+            padding_top = Math.floor((height - img_height) / 2)
+            $img.css('width', '100vw');
+            $img.css('height', 'auto');
+            $img.css('padding-left', 0);
+        }
+    };
+
+    $(function() {
+        $container = $('#fullscreen_container');
+        $img = $container.find('img');
+        resize();
+        $(window).resize( resize );
+        return $(window).scroll(on_scroll);
+        // return window.requestAnimationFrame(on_scroll);
+    });
+
+})();
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Label from '$lib/components/Label.svelte'
+	import Project from '$lib/components/Project.svelte'
+	import HeadTags from '$lib/components/HeadTags.svelte'
+	import { labels, projects } from './data'
+	import { writable, type Writable } from 'svelte/store'
+
+	const selectedLabel: Writable<string | null> = writable(null)
+</script>
+
+<HeadTags
+	title="Joel Simon"
+	description="Joel Simon Website portfolio project"
+	imagePath="/imgs/cornered-hands/1.jpg"
+	type="blog"
+/>
+
+<h1>Joel Simon</h1>
+<div id="label-container">
+	{#each labels as label}
+		{#key label}
+			<Label {label} {selectedLabel} />
+		{/key}
+	{/each}
+</div>
+
+<div id="project-container">
+	{#each projects as project}
+		{#key project.name}
+			<Project {project} {selectedLabel} />
+		{/key}
+	{/each}
+</div>
