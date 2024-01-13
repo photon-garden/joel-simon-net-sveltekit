b85f646bd9f140b992d3b5f5a9c99634974c3386
Luca Bailey <mail@photon.garden>
2024-01-13 10:35:45 -0500

    Add console.debugs to [legacyProjectSlug]/+page.ts.

--- src/routes/[legacyProjectSlug]/+page.ts
+++ src/routes/[legacyProjectSlug]/+page.ts
@@ -3,7 +3,7 @@ import * as Projects from '$lib/Projects'
 import * as StatusCodes from '$lib/StatusCodes'
 import { error } from '@sveltejs/kit'
 
-// Tell SvelteKit about all the legacy projects.
+// Tell SvelteKit about all the legacy projects so it knows to prerender them.
 /** @type {import('./$types').EntryGenerator} */
 export function entries() {
 	return projects.map((project) => {
@@ -16,13 +16,16 @@ export function entries() {
 /** @type {import('./$types').PageLoad} */
 export function load({ params }) {
 	const desiredSlug = params.legacyProjectSlug
+	console.debug('desiredSlug', desiredSlug)
 
 	const project = projects.find((project) => {
 		const slug = Projects.getSlug(project)
 		return slug === desiredSlug
 	})
+	console.debug('project', project)
 
 	if (project == null) {
+		console.error('No project found with slug', desiredSlug)
 		throw error(StatusCodes.notFound)
 	}
 
